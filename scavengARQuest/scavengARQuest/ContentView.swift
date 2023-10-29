b811518e8a16e3c6c1a73f0d8ab323fe7aa5c74a
atooman <atooman@umich.edu>
2023-10-29 16:42:48 -0400

    top part of home page

--- scavengARQuest/scavengARQuest/ContentView.swift
+++ scavengARQuest/scavengARQuest/ContentView.swift
@@ -1,24 +1,35 @@
-//
-//  ContentView.swift
-//  ScavengARQuest
-//
-//  Created by Zachary Weiss on 10/29/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchText = "" // Define and declare searchText as a @State property
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("ScavangAR")
+                    .font(.headline)
+                Spacer()
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+            }
+            Text("Ann Arbor, MI")
+                .font(.subheadline)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            TextField("Search for quest...", text: $searchText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            Spacer()
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
