7aab796b3d394339c815f919fa71b20ad91b4a7b
zjweiss <zjweiss@umich.edu>
2023-10-29 13:55:47 -0400

    initial xcode files

---
+++ scavengARQuest/scavengARQuest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScavengARQuest
+//
+//  Created by Zachary Weiss on 10/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ scavengARQuest/scavengARQuest/scavengARQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScavengARQuestApp.swift
+//  ScavengARQuest
+//
+//  Created by Zachary Weiss on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ScavengARQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
