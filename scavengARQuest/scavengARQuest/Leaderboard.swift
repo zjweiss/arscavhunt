5c310541510f6f23091d9d21864bb523f903d547
Jenny-233 <51002233+Jenny-233@users.noreply.github.com>
2023-11-01 18:04:19 -0400

    leaderboard mvp w/o backend

--- scavengARQuest/scavengARQuest/Leaderboard.swift
+++ scavengARQuest/scavengARQuest/Leaderboard.swift
@@ -8,10 +8,128 @@
 import SwiftUI
 
 struct LeaderboardPage: View {
+    let leaderBoard: [LeaderboardInfo] = [
+        LeaderboardInfo(
+             id: "1",
+             name: "Kendall Jenner",
+             place: "1",
+             points: "188",
+             image: "user_head"
+        ),
+        LeaderboardInfo(
+             id: "2",
+             name: "Abbie Tooman",
+             place: "2",
+             points: "130",
+             image: "user_head"
+        ),
+        LeaderboardInfo(
+             id: "3",
+             name: "Zach Weis",
+             place: "3",
+             points: "92",
+             image: "user_head"
+        ),
+        LeaderboardInfo(
+             id: "4",
+             name: "Jane Doe",
+             place: "4",
+             points: "90",
+             image: "user_head"
+        )
+
+    ]
+    
+    let curUserLeaderboard: LeaderboardInfo =
+        LeaderboardInfo(
+        id: "1",
+        name: "Jane Doe",
+        place: "4",
+        points: "90",
+        image: "user_head")
+    
+    
     var body: some View {
-        ZStack{
-            Text("This is the Leaderboard Page")
-        }
+            NavigationView {
+                ScrollView {
+                    VStack {
+                        HStack {
+                            Button(action: {
+                                // TODO: add back button action
+                            }) {
+                                Image(systemName: "chevron.left")
+                                    .scaleEffect(0.6)
+                                    .font(Font.title.weight(.medium))
+                            }
+                            Spacer()
+                            Image(systemName: "app.gift")
+                                .imageScale(.large)
+                                .foregroundStyle(.tint)
+                            Image(systemName: "bell")
+                                .imageScale(.large)
+                                .foregroundStyle(.tint)
+                        }
+                        Spacer()
+                        Text("Leaderboard")
+                            .font(.custom("Times New Roman", size: 30))
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Spacer()
+                        Text(curUserLeaderboard.name ?? "")
+                            .font(.custom("Times New Roman", size: 25))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        HStack {
+                            VStack{
+                                HStack{
+                                    Text(curUserLeaderboard.place ?? "-1")
+                                        .font(.custom("Times New Roman", size: 30))
+                                        .fontWeight(.bold)
+                                    Text("th")
+                                        .font(.custom("Times New Roman", size: 30))
+                                        .fontWeight(.bold)
+                                }
+                                Text("Place")
+                                    .font(.custom("Times New Roman", size: 12))
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.gray) // Set the text color to gray
+                                    .frame(maxWidth: .infinity, alignment: .center)
+                            }
+                            VStack{
+                                Spacer()
+                                Image(curUserLeaderboard.image)
+                                    .resizable()
+                                    .frame(width: 100, height: 100) // Adjust the size as needed
+                                    .clipShape(Circle())
+                                    .overlay(Circle().stroke(Color.blue, lineWidth: 2)) // Optional: Add a border
+                                    .shadow(radius: 5) // Optional: Add a shadow
+                                Spacer()
+                            }
+                            VStack{
+                                Text(curUserLeaderboard.points ?? "0")
+                                    .font(.custom("Times New Roman", size: 30))
+                                    .fontWeight(.bold)
+                                    .frame(maxWidth: .infinity, alignment: .center)
+                                Text("Points")
+                                    .font(.custom("Times New Roman", size: 12))
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.gray) // Set the text color to gray
+                                    .frame(maxWidth: .infinity, alignment: .center)
+                            }
+                        }
+                        Divider() // Create a horizontal line
+                        Spacer()
+                        Text("Ranking")
+                            .font(.custom("Times New Roman", size: 20))
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        // TODO: figure out how to push pic/name to the left
+                        LeaderboardInfoContent(leaderboard: leaderBoard)
+                            .frame(maxWidth: .infinity)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .padding()
+                }
+            }
     }
 }
 
---
+++ scavengARQuest/scavengARQuest/LeaderboardRow.swift
@@ -0,0 +1,63 @@
+//
+//  LeaderboardRow.swift
+//  ScavengARQuest
+//
+//  Created by Janice Liu on 11/1/23.
+//
+
+import SwiftUI
+
+struct LeaderboardInfo: Codable, Hashable, Identifiable {
+
+    let id: String?
+    let name: String?
+    let place: String?
+    let points: String?
+    let image: String
+    
+    static func preview() -> LeaderboardInfo {
+        LeaderboardInfo(
+             id: "1",
+             name: "Jane Doe",
+             place: "4th",
+             points: "90",
+             image: "user_head"
+        )
+    }
+}
+
+struct LeaderboardInfoContent: View {
+    let leaderboard: [LeaderboardInfo]
+
+    var body: some View {
+        ForEach(leaderboard, id: \.id) {
+            player in
+            VStack {
+                HStack{
+                    if let place = player.place {
+                        Text(place)
+                            .font(.subheadline)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    Image(player.image)
+                        .resizable()
+                        .frame(width: 25, height: 25) // Adjust the size as needed
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.blue, lineWidth: 2)) // Optional: Add a border
+                        .shadow(radius: 5) // Optional: Add a shadow
+                    if let name = player.name {
+                        Text(name)
+                            .font(.subheadline)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    Spacer()
+                    if let points = player.points {
+                        Text(points)
+                            .font(.subheadline)
+//                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                }
+            }
+        }
+    }
+}
