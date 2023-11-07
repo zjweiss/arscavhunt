10c9b3b5229649d850fe7f8f714839b32968e056
Jenny-233 <51002233+Jenny-233@users.noreply.github.com>
2023-11-07 10:48:04 -0500

    leaderboard merge

--- scavengARQuest/scavengARQuest/LeaderboardRow.swift
+++ scavengARQuest/scavengARQuest/LeaderboardRow.swift
@@ -26,35 +26,40 @@
  }
 
  struct LeaderboardInfoContent: View {
-     let leaderboard: [LeaderboardInfo]
+    let leaderboard: [LeaderboardInfo]
 
-     var body: some View {
-         ForEach(leaderboard, id: \.id) {
-             player in
-             VStack {
-                 HStack{
-                     if let place = player.place {
-                         Text(place)
-                             .font(.subheadline)
-                     }
-                     Image(player.image)
-                         .resizable()
-                         .frame(width: 25, height: 25) // Adjust the size as needed
-                         .clipShape(Circle())
-                         .overlay(Circle().stroke(Color.blue, lineWidth: 2)) // Optional: Add a border
-                         .shadow(radius: 5) // Optional: Add a shadow
-                     if let name = player.name {
-                         Text(name)
-                             .font(.subheadline)
-                             .frame(maxWidth: .infinity, alignment: .leading)
-                     }
-                     Spacer()
-                     if let points = player.points {
-                         Text(points)
-                             .font(.subheadline)
-                     }
-                 }
-             }
-         }
-     }
- }
+    var body: some View {
+        ForEach(leaderboard, id: \.id) {
+            player in
+            VStack {
+                HStack{
+                    if let place = player.place {
+                        Text(place)
+                            .font(.custom("Times New Roman", size: 20))
+                            .padding(.horizontal, 5) // Add vertical padding between each player
+                    }
+                    Image(player.image)
+                        .resizable()
+                        .frame(width: 35, height: 35) // Adjust the size as needed
+                        .clipShape(Circle())
+                        .padding(.horizontal, 5) // Add vertical padding between each player
+                        .shadow(radius: 5) // Optional: Add a shadow
+                    if let name = player.name {
+                        Text(name)
+                            .font(.custom("Times New Roman", size: 20))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.horizontal, 5) // Add vertical padding between each player
+
+                    }
+                    Spacer()
+                    if let points = player.points {
+                        Text(points)
+                            .font(.custom("Times New Roman", size: 20))
+                            .padding(.horizontal, 5) // Add vertical padding between each player
+                    }
+                }
+            }
+            .padding(.vertical, 5) // Add vertical padding between each player
+        }
+    }
+}
