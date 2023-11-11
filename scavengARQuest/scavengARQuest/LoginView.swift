0c8b0bbc632e744d8baa7365c089eda597088d4a
zjweiss <zjweiss@umich.edu>
2023-11-11 16:11:03 -0500

    added more login stuff to user preferences dict

--- scavengARQuest/scavengARQuest/LoginView.swift
+++ scavengARQuest/scavengARQuest/LoginView.swift
@@ -63,9 +63,13 @@ struct LoginView: View {
                                let firstName = userArray[1] as? String ?? ""
                                let lastName = userArray[2] as? String ?? ""
                                let userID = userArray[0] as? Int ?? 0
+                               let points = userArray[4] as? Int ?? 0
                                let canolicalName = firstName + " " + lastName
                                defaults.set(canolicalName, forKey: "canName")
                                defaults.set(userID, forKey: "userID")
+                               defaults.set(firstName, forKey: "firstName")
+                               defaults.set(lastName, forKey: "lastName")
+                               defaults.set(points, forKey: "points")
 
                            }
                            defaults.set(username, forKey: "logname")
