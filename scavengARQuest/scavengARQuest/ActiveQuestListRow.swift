e5fc1b7223a4b01c250354f298bbe0b066f1dd00
Rohan Nagavardhan <rohans415@gmail.com>
2023-11-11 21:44:15 -0500

    struggling with positioning the checkbox and text box independently

--- scavengARQuest/scavengARQuest/ActiveQuestListRow.swift
+++ scavengARQuest/scavengARQuest/ActiveQuestListRow.swift
@@ -7,6 +7,39 @@
 
 import SwiftUI
 
+struct ActiveQuestLocationCard: View {
+    let data: Location
+    
+    @ViewBuilder
+    func Checkbox() -> some View {
+        let isComplete: Bool = data.status == "complete"
+        
+        Image(systemName: isComplete ? "checkmark.circle.fill" : "circle")
+            .resizable()
+            .frame(width: 25, height: 25)
+            .foregroundColor(isComplete ? Color.green : Color.white)
+    }
+    
+    var body: some View {
+        ZStack {
+            Image("dow_building")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: .infinity)
+                .foregroundStyle(.tint)
+                .cornerRadius(10.0)
+
+            RoundedRectangle(cornerRadius: 10.0)
+                .foregroundColor(Color.black.opacity(0.30))
+            
+            Text(data.name)
+                .font(.system(size: 24))
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+        }
+    }
+}
+
 struct ActiveQuestInfo: Codable, Hashable, Identifiable {
 
     let id: String?
--- scavengARQuest/scavengARQuest/scavengARQuestApp.swift
+++ scavengARQuest/scavengARQuest/scavengARQuestApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct ScavengARQuestApp: App {
+    @State var questId: Int = 1
+    @State var questName: String = "Campus Study Spots"
+    @State var complete: Int = 1
+    @State var incomplete: Int = 6
+    
     var body: some Scene {
         WindowGroup {
-        LoginView()
+            ActiveQuestPage(
+                questId: $questId,
+                questName: $questName,
+                incomplete: $incomplete,
+                complete: $complete
+            )
         }
     }
 }
