9183309fd9bfcbb61a64e09879795054d80c0bc2
Rohan Nagavardhan <rohans415@gmail.com>
2023-11-18 23:29:28 -0500

    updated leaderboard ui

--- scavengARQuest/scavengARQuest/LeaderboardRow.swift
+++ scavengARQuest/scavengARQuest/LeaderboardRow.swift
@@ -7,28 +7,30 @@
  import SwiftUI
 
  struct LeaderboardRow: View {
-     var index: Int
      var currentUserId: Int
+     
      var user: User
      
-    var body: some View {
-        HStack(alignment: .center) {
-            Text(String(index+1))
-                .font(.system(size: 20).monospacedDigit())
-                .foregroundStyle(Color.gray)
-                .padding(.trailing, 8)
-            
-            Text(user.firstName + " " + user.lastName)
-                .font(.system(size: 20))
-            
-            Spacer()
-            
-            Text(user.totalPoints)
-                .font(.system(size: 25))
-                .fontWeight(.semibold)
-        }
-        .padding(8)
-        .background(currentUserId == user.userId ? Color.yellow.opacity(0.1) : nil)
-        .cornerRadius(10.0)
-    }
+     let backgroundColor = Color(red: 23/255, green: 37/255, blue: 84/255)
+     
+     var body: some View {
+         HStack(alignment: .center) {
+             Text(String(user.ranking))
+                 .font(.system(size: 20).monospacedDigit())
+                 .foregroundStyle(Color.gray)
+                 .padding(.trailing, 8)
+             
+             Text(user.firstName + " " + user.lastName)
+                 .font(.system(size: 20))
+             
+             Spacer()
+             
+             Text(user.totalPoints)
+                 .font(.system(size: 25))
+                 .fontWeight(.semibold)
+         }
+         .padding(8)
+         .background(currentUserId == user.userId ? backgroundColor.opacity(0.15) : nil)
+         .cornerRadius(10.0)
+     }
 }
