4777bfa73739f0ff988fe884cb5434f36cc88651
Rohan Nagavardhan <rohans415@gmail.com>
2023-11-27 10:09:22 -0500

    add locations

--- db/campus-murals.sql
+++ db/campus-murals.sql
@@ -1,27 +1,41 @@
-INSERT INTO tags (name)
-VALUES
-  ('Art'),
-  ('Sculpture'),
-  ('Downtown')
-  ('Kerrytown'),
-  ('Food');
-
-
+INSERT INTO tags (name);
+  
 INSERT INTO locations (name, latitude, longitude, description, thumbnail, distance_threshold)
 VALUES
-  ('Challenge Everything. Create Anything', 42.28106, -83.74745, '', '', 50)
-  ('Symbiosis', 42.28223, -83.75118, '', '', 50),
-  ('Not Afraid to Dance', 42.28010, -83.75129, '', '', 50),
-  ('The Globin Dokebi', 42.28020, -83.75129, '', '', 50),
-  ('Orion', 42.27567, -83.23982, '', '', 50),
-  ('The Cube', 42.275600, -83.742410, '', '', 50),
-  ('The Rock', 42.272420, -83.728040, '', '', 50),
-  ('Pat Perry Mural', 42.280750, -83.749440, '', '', 50),
-  ('Jesse Kassel Mural', 42.2794956, -83.7482955, '', '', 50),
-  ('Dylan Strzynski Mural', 42.279430, -83.749330, '', '', 50),
-  ('LOOMROOM', 42.276031, -83.751060, '', '', 50),
-  ('Jacob Dwyer Mural', 42.281980, -83.751170, '', '', 50),
-  ('Nina Shahnin Mural', 42.279610, -83.748500, '', '', 50),
+  ('Challenge Everything. Create Anything',	42.2811408716837, -83.74752358, 'Behold the captivating "Challenge Everything. Create Anything." mural, a testament to the dynamic collaboration between Destination Ann Arbor, Wickfield Properties, and esteemed local artists Mary Thiefels and Danijel Matanic. Adorning the 10-story wall of Courthouse Square at 100 South 4th Avenue, this vibrant masterpiece, completed in the summer of 2019, radiates Ann Arbor''s spirited essence and tight-knit community spirit. The mural invites onlookers to embrace human creativity, ingenuity, and curiosity through its playful and illustrative depiction, earning widespread acclaim and becoming a cherished symbol of the city''s cultural vitality.', '', 50),
+
+  ('Bookstore Mural',	42.277980252844955, -83.74215417808378, 'The Bookstore Mural is a large outdoor mural measuring approximately 60 feet by 20 feet that features the portraits of five cultural icons: Woody Allen, Edgar Allan Poe, Hermann Hesse, Franz Kafka, and Anais Nin. It has also been known as the Poet Mural, Liberty Street Mural, and East Liberty Street Wall Mural. The mural was painted in 1984 by artist Richard Wolk and has since become an iconic landmark in Ann Arbor.', '', 50),
+
+  ('Not Afraid to Dance',	42.28017185472255, -83.75112929260827, 'The mural by Gary Horton, titled "Not Afraid to Dance," depicts several bright figures dancing together and was commissioned in March 2020, just as the world was shutting down due to the COVID-19 pandemic. The artist created it as a reminder of how he and his wife found comfort through continuing their small, weekly dance parties at home during the scary early days of restrictions and lockdowns. Commissioned by Circ Bar, it commemorates finding joy in simple acts during difficult times.', '', 50),
+
+  ('Dokebi'	42.28030056817844, -83.75116013186488, 'The mural features a dokkaebi, a goblin-like trickster from Korean folklore known for playing pranks on humans. The artist Chris "Dokebi" Sammons, who takes his moniker from this creature, brings it to life through his painting in vivid colors and playful composition. His distinctive interpretation of the folklore character embodies its spirit of fun and mischief through an animated scene that will engage and entertain viewers.', '', 50),
+
+  ('Pride in Patterns',	42.278932232589604, -83.74928717116414, 'Metro Detroit artist Joey Salamon''s mural combines eye-catching patterns of repeating geometric shapes in a rainbow of colors, with dimensions added by the use of different hues and tones. His signature style exhibits vibrant abstract forms and clean lines that enhance the graphic feel of the artwork. The piece aims to exude joyfulness and pride for the community through dimensional space and a pop of vibrant colors.', '', 50),
+
+  ('Coyotes in the City',	42.27974790760839, -83.74960379816177, 'Rendered in a style mirroring the artist''s wall installations, the mural depicts typical suburban motifs like houses and fences alongside an urbanized coyote. Along with other modern icons, these images have been translated from Dylan Strzynski''s usual works to the concrete canvas in a manner reflective of how wildlife adjusts to city life. Viewers familiar with the artist''s focus on architecture and nature will recognize references adapted here for their downtown setting.', '', 50),
+
+  ('Community Connections', 42.27912648435359, -83.75194659814942, 'Featuring large insects and plants representing the interconnected relationships between Ann Arbor and Ypsilanti. Artist Yen Azzaro depicted the local agriculture and ecosystems binding the communities through bright colors, whimsical details, and symbolic butterflies and bees. Through visual and written metaphor, the mural celebrated regional bounty, diversity, and the thriving network of partnerships across communities.', '', 50),
+
+  ('Humanity and Technology', 42.280612367464414, -83.75115402698519, 'Featuring hands exploring the relationship between people and digital devices against a backdrop of horizontal lines cutting through the composition. The artist Taylor White used figurative imagery and technology as themes to represent how humanity is inherently intertwined with advancing tools. Scanners and code peek out from between the fingers to symbolize how social media can both connect and threaten civilization.', '', 50),
+
+  ('Symbiosis Of The Red Bellied Woodpecker and the Eastern Bluebird',	42.28237381560644, -83.7511685423284, 'Dive into the vibrant depiction of the interconnected tale of a red-bellied woodpecker and an Eastern bluebird. This expansive artwork unfolds the woodpecker''s role in providing a nest cavity in an oak tree, emphasizing the captivating dance of local wildlife and their interwoven destinies.', '', 50),
+
+  ('Midnight Olive',	42.28197400393415, -83.7507180460296, 'Created by Detroit-based artist Olivia Guterson, who goes by the name Midnight Olive. Deeply influenced by her Jewish and Black heritage, her works accentuate the balance of black and white, busy and still areas through an interwoven network of lines and shapes. Guterson''s instinctive linework in this piece is meant to embed feelings of love, hope, and joy in the places she leaves her mark. She describes her artistic process as "art as a moving meditation" that facilitates her own healing and truth-seeking. Visitors to the site will be struck by the complexity and balance achieved through Guterson''s interdisciplinary approach to composition and structure in this large-scale public artwork.', '', 50),
+
+  ('May Her Memory Be Our Innermost Revolution', 42.28226903959532, -83.750133755821, 'The vibrantly colored mural depicts a larger-than-life portrait of Ann Lewis, a multidisciplinary Detroit artist known for using public murals, installations and participatory performances to bring attention to topics like gentrification, women''s rights, and police brutality. Her distinctive graphic style is evident in the repetitive geometric patterns that form the background behind her image. Lewis'' thought-provoking gaze engages viewers as her message of empowerment and intersectional social justice advocacy shines through. Created in collaboration with members of the local community, the mural highlights the shared humanity in all people, especially those who are often marginalized like women transitioning from incarceration back into society. Visitors will be struck by the mural''s aesthetically pleasing design and moved by the important issues it illuminates through Lewis'' inspiring artwork.', '', 50),
+
+  ('',	42.28248307674222, -83.7495937711642, '', '', 50),
+
+  ('Pat Perry',	42.28095411564499, -83.74956765582102, '', '', 50),
+
+  ('Drifts'	42.28059389456204, -83.74990334047781, '', '', 50),
+
+  ('',	42.279848496533994, -83.74782372513464, '', '', 50),
+
+  ('',	42.279578573199444, -83.74782499999999, '', '', 50),
+
+  ('The Glass Beads',	42.27700135727122, -83.748233188358, '', '', 50);
+
 
 
 
