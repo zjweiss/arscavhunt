00ba776dc36d2a4916f3a1831028a55246f133d1
Rohan Nagavardhan <rohans415@gmail.com>
2023-11-26 20:06:31 -0500

    new campus murals db script

---
+++ db/campus-murals.sql
@@ -0,0 +1,27 @@
+INSERT INTO tags (name)
+VALUES
+  ('Art'),
+  ('Sculpture'),
+  ('Downtown')
+  ('Kerrytown'),
+  ('Food');
+
+
+INSERT INTO locations (name, latitude, longitude, description, thumbnail, distance_threshold)
+VALUES
+  ('Challenge Everything. Create Anything', 42.28106, -83.74745, '', '', 50)
+  ('Symbiosis', 42.28223, -83.75118, '', '', 50),
+  ('Not Afraid to Dance', 42.28010, -83.75129, '', '', 50),
+  ('The Globin Dokebi', 42.28020, -83.75129, '', '', 50),
+  ('Orion', 42.27567, -83.23982, '', '', 50),
+  ('The Cube', 42.275600, -83.742410, '', '', 50),
+  ('The Rock', 42.272420, -83.728040, '', '', 50),
+  ('Pat Perry Mural', 42.280750, -83.749440, '', '', 50),
+  ('Jesse Kassel Mural', 42.2794956, -83.7482955, '', '', 50),
+  ('Dylan Strzynski Mural', 42.279430, -83.749330, '', '', 50),
+  ('LOOMROOM', 42.276031, -83.751060, '', '', 50),
+  ('Jacob Dwyer Mural', 42.281980, -83.751170, '', '', 50),
+  ('Nina Shahnin Mural', 42.279610, -83.748500, '', '', 50),
+
+
+
